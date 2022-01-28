852d437b303990a99e9d2fe29e4202a380ab9b93
NTX0325 <ad0124578963@gmail.com>
2022-01-28 12:44:53 +0800

    Update countp.js

--- webpage/testexample/js/countp.js
+++ webpage/testexample/js/countp.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
-   $.getJSON("./other/count_v1.json", function(data){
-            console.log(data.count); // Prints: Harry
-        }).fail(function(){
-            console.log("An error has occurred.");
-        }); 
+    $.getJSON("./other/count_v1.json", function(data){
+        data.count+=1;
+        $("#count").html(data.count);
+        console.log(data.count); // Prints: Harry
+    }).fail(function(){
+        console.log("An error has occurred.");
+    }); 
 });
